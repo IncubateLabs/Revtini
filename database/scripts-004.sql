40aa6912ea45d60617fcd1d1f54893c50aa1a4cb
unknown <janakadalugama@gmail.com>
2013-08-09 16:46:51 +0530

    sample data added for merchants

---
+++ database/scripts-004.sql
@@ -0,0 +1,27 @@
+insert into merchants (`merchants`.`mId`,
+`merchants`.`code`,
+`merchants`.`company_name`,
+`merchants`.`phone`,
+`merchants`.`email`,
+`merchants`.`date_created`) values (1,'Arpico','Arpico pvt ltd','0112 987432','info@apico.com','2013/08/12');
+
+insert into merchants (`merchants`.`mId`,
+`merchants`.`code`,
+`merchants`.`company_name`,
+`merchants`.`phone`,
+`merchants`.`email`,
+`merchants`.`date_created`) values (2,'Jinasena','Ginasena pvt ltd','0112 956532','info@ginasena.com','2013/07/15');
+
+insert into merchants (`merchants`.`mId`,
+`merchants`.`code`,
+`merchants`.`company_name`,
+`merchants`.`phone`,
+`merchants`.`email`,
+`merchants`.`date_created`) values (3,'Browns','Browns pvt ltd','0112 987432','info@browns.com','2013/06/12');
+
+insert into merchants (`merchants`.`mId`,
+`merchants`.`code`,
+`merchants`.`company_name`,
+`merchants`.`phone`,
+`merchants`.`email`,
+`merchants`.`date_created`) values (4,'MagCity','Magcity pvt ltd','0112 908765','info@mag.com','2011/08/12');
