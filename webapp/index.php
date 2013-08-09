42c53d86a13052f677e41ff164abc55b343aa8a6
Suba <subhashiniprarthanavithanage@gmail.com>
2013-08-09 17:23:05 +0600

    addlogo

--- webapp/index.php
+++ webapp/index.php
@@ -19,7 +19,7 @@
     <div class="container">
 
       <div class="masthead">
-        <h3 class="text-muted">Revtini</h3>
+        <img src="Logo.jpg" height="100px" width="100px">
         <div class="navbar">
           <!-- <a class="navbar-brand" href="#"></a> -->
           <ul class="nav navbar-nav">
